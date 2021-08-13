0033fa85225d28a483dd3972d0bf57a33df94e6b
Varun Ragunath <varunragu23@gmail.com>
2021-08-13 14:51:43 -0700

    Implemented DriveTrain subsystem, and initialized RobotContainer

--- command-robot-challenge/src/main/java/frc/robot/Constants.java
+++ command-robot-challenge/src/main/java/frc/robot/Constants.java
@@ -12,4 +12,25 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public static final int LEFT_FRONT = 3;
+	public static final int RIGHT_FRONT = 1;
+	public static final int LEFT_BACK = 4;
+    public static final int RIGHT_BACK = 2;
+    
+    public static final boolean CLOCKWISE = false;
+    public static final boolean COUNTERCLOCKWISE = true;
+
+    public static final int XBOX_LEFT_X_AXIS = 0;
+    public static final int XBOX_LEFT_Y_AXIS = 1;
+
+    public static final double DRIVETRAINSPEED = 0.7;
+
+    public static final int JOYSTICK_NUMBER = 0;
+
+    public static final int kLeftX = 2;
+    public static final int kRightX = 4;
+    public static final int kLeftY = 1;
+    public static final int kRightY = 5;
+}
--- command-robot-challenge/src/main/java/frc/robot/RobotContainer.java
+++ command-robot-challenge/src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,9 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.DriveWithJoystick;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -17,7 +19,13 @@ import edu.wpi.first.wpilibj2.command.Command;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+
+  private final DriveTrain driveTrain;
+  private final DriveWithJoystick driveWithJoystick;
+
+  public static XboxController driverJoystick;
+
+// The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
@@ -25,7 +33,18 @@ public class RobotContainer {
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
+
+    driveTrain = new DriveTrain();
+    driveWithJoystick = new DriveWithJoystick(driveTrain);
+    driveWithJoystick.addRequirements(driveTrain);
+    driveTrain.setDefaultCommand(driveWithJoystick);
+
+    driverJoystick = new XboxController(Constants.JOYSTICK_NUMBER);
+
+
     configureButtonBindings();
+
+
   }
 
   /**
@@ -34,9 +53,11 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    
+  }
 
-  /**
+  /**~
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
